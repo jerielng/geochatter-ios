3b3fb4f17a799f159aad81a82fe450bc3f87ed65
Jeriel <jeriel.ng@gmail.com>
2020-05-12 23:59:16 -0400

    Added LoginViewController files

---
+++ Geochatter/View Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Geochatter
+//
+//  Created by Jeriel Ng on 5/12/20.
+//  Copyright © 2020 Jeriel Ng. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
